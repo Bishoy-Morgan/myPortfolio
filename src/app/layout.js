bbfc8b3a06b2f2c1a3242e000cbbec27b2690f47
Bishoy Morgan <bishoy.morgan95@gmail.com>
2025-08-03 06:46:44 +0300

    layout-edits

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import MobileNavbar from "./components/MobileNavrbar";
 import Head from "next/head";
+import Script from "next/script";
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -15,11 +16,35 @@ export const metadata = {
   description: "Bishoy Morgan Services and Portfolio",
 };
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="32x32"></link>
+        {GA_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            />
+            <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body className={`${manrope.variable} antialiased`}>
         <div className="hidden lg:block">
